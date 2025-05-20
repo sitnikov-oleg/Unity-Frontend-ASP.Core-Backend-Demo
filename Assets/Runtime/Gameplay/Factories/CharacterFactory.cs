ab8d2ca898f8f83c7fbbb0a4a63ca4387afb9148
ositnikov <mr.moonshield@yandex.ru>
2025-05-20 11:02:43 +0300

    вынес у настройку спауна в константы

--- Assets/Runtime/Gameplay/Factories/CharacterFactory.cs
+++ Assets/Runtime/Gameplay/Factories/CharacterFactory.cs
@@ -7,6 +7,7 @@ public class CharacterFactory
 {
 	[Inject] private readonly SceneReferences sceneReferences;
 	[Inject] private readonly SharedObjects sharedObjects;
+	private const float Y_SPAWN_POSITION = 4;
 
 	public IEnumerable<AbstractUnit> Spawn(CharacterType characterType, int amount)
 	{
@@ -19,7 +20,7 @@ public class CharacterFactory
 		{
 			var posX = Random.Range(bounds.min.x, bounds.max.x);
 			var posZ = Random.Range(bounds.min.z, bounds.max.z);
-			NavMesh.SamplePosition(new Vector3(posX, 4, posZ), out var hit, 10, NavMesh.AllAreas);
+			NavMesh.SamplePosition(new Vector3(posX, Y_SPAWN_POSITION, posZ), out var hit, 10, NavMesh.AllAreas);
 			var rot = Quaternion.Euler(0, Random.Range(0, 360), 0);
 			var character = Object.Instantiate(prefab, hit.position, rot, folder).GetComponent<AbstractUnit>();
 			charactersList.Add(character);
